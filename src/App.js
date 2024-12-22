330eaee475bef2ae9a9ba145be1f26c2344bd9e0
Duc3rd <165653481+Duc3rd@users.noreply.github.com>
2024-12-22 08:48:55 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AddProduct from './components/AddProduct';
+import EditProduct from './components/EditProduct';
+import ProductList from './components/ProductList';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/add-product" component={AddProduct} />
+          <Route path="/edit-product/:id" component={EditProduct} />
+          <Route path="/" component={ProductList} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
